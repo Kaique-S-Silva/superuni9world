12690f32b2406e135692921e8984c22539a69e91
Kaique S Silva <k16ksds@gmail.com>
2026-05-07 15:28:59 +0000

    Adicionado abas: Contato, Wiki.; CSS mais responsivo; Design do site modificado.

--- components/header.php
+++ components/header.php
@@ -3,8 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <title>Super UNI9 World</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" rel="stylesheet">
+    
     <link rel="stylesheet" href="assets/style.css">
+
+<nav>
+    <a href="index.php?aba=homepage">Início</a>
+    <a href="index.php?aba=game">Jogar</a>
+    <a href="index.php?aba=wiki">Wiki</a>
+    <a href="index.php?aba=contact">Contato</a>
+</nav>
 </head>
-<body>
-    <header>
-    </header>
--- pages/game.php
+++ pages/game.php
@@ -3,8 +3,8 @@
     <iframe frameborder="0" 
         src="https://itch.io/embed-upload/17423334?color=333333" 
         allowfullscreen="" 
-        width="800" 
-        height="600">
-        <a href="https://merthiz.itch.io/my-game">Hosted by Itch.io</a>
+        width="640" 
+        height="360">
+        <a href="https://merthiz.itch.io/superuni9world">Hosted by Itch.io</a>
     </iframe>
 </section>
