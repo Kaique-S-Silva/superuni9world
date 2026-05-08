e32b37436d11e3344c5fc3e562387582e6d07a55
Kaique S Silva <k16ksds@gmail.com>
2026-05-08 12:24:35 +0000

    ajustado design da pagina do jogo

--- pages/game.php
+++ pages/game.php
@@ -1,10 +1,16 @@
 <!-- Área do Jogo -->
 <section id="game-area">
+    <div class="game-title">
+        <h2>Super UNI9 World</h2>
+        <p>Jogue agora mesmo o nosso incrível jogo de plataforma 2D, disponível gratuitamente no Itch.io! 
+            Mergulhe em um mundo cheio de desafios, inimigos e segredos para descobrir. Jogue e divirta-se!</p>
+    </div>
+    
     <iframe frameborder="0" 
         src="https://itch.io/embed-upload/17423334?color=333333" 
         allowfullscreen="" 
-        width="640" 
-        height="360">
+        width="100%"
+        height="750">
         <a href="https://merthiz.itch.io/superuni9world">Hosted by Itch.io</a>
     </iframe>
 </section>
