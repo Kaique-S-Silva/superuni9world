c4f732be7f97dff27fde0651e973fc8c8f72514b
Kaique S Silva <k16ksds@gmail.com>
2026-05-08 14:18:13 +0000

    alterado design do header

--- components/header.php
+++ components/header.php
@@ -6,13 +6,18 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" rel="stylesheet">
-    
     <link rel="stylesheet" href="assets/style.css">
-
-<nav>
-    <a href="index.php?aba=homepage">Início</a>
-    <a href="index.php?aba=game">Jogar</a>
-    <a href="index.php?aba=wiki">Wiki</a>
-    <a href="index.php?aba=contact">Contato</a>
-</nav>
 </head>
+<body>
+    <header>
+        <div class="header-inner">
+            <a class="brand" href="index.php?aba=homepage">Super UNI9 World</a>
+            <nav aria-label="Menu principal">
+                <a href="index.php?aba=homepage">Início</a>
+                <a href="index.php?aba=game">Jogo</a>
+                <a href="index.php?aba=wiki">Wiki</a>
+                <a href="index.php?aba=contact">Contato</a>
+            </nav>
+        </div>
+    </header>
+</body>
