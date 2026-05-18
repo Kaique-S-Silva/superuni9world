5301a1e2c27ad9e21ad070e822ee3bafa0fead2b
Kaique S Silva <k16ksds@gmail.com>
2026-05-18 11:58:54 +0000

    algumas coisas ai

--- resources/views/pages/game.php
+++ resources/views/pages/game.php
@@ -6,11 +6,12 @@
             Mergulhe em um mundo cheio de desafios, inimigos e segredos para descobrir. Jogue e divirta-se!</p>
     </div>
     
-    <iframe frameborder="0" 
-        src="https://itch.io/embed-upload/17423334?color=333333" 
-        allowfullscreen="" 
-        width="100%"
-        height="750">
-        <a href="https://merthiz.itch.io/superuni9world">Hosted by Itch.io</a>
+    <iframe 
+    frameborder="0" 
+    src="https://itch.io/embed-upload/17423334?color=000000"
+    width="1100" 
+    height="700">
+    <a href="https://merthiz.itch.io/superuni9world">Play my-game on itch.io</a>
     </iframe>
+    
 </section>
